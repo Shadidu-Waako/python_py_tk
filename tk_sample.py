42e3ced9b9f6ed29133d5a28be1763074845614a
Waako Shadidu Ismail <123797767+Shadidu-Waako@users.noreply.github.com>
2023-10-23 15:47:36 +0300

    Create tk_sample.py

---
+++ tk_sample.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+from tkinter import ttk
+
+window = tk.Tk()
+window.configure(background=Light Blue")
+window.title("simple addition")
+
+textbox1 = ttk.Entry(container, **options)
+textbox1.get()
+
+textbox1 = ttk.Entry(container, **options)
+textbox1.get()
+
+
+window.mainloop()
